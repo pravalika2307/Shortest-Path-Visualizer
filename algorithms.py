4da7870597395566b537d29465c32aae1fecd869
Pravalika <pravalikareddy315@gmail.com>
2026-06-05 12:20:19 +0530

    Binary search programs

--- algorithms.py
+++ algorithms.py
@@ -5,11 +5,11 @@ def get_neighbors(node, rows, cols):
     r, c = node
 
     directions = [
-        (-1,0),
-        (1,0),
-        (0,-1),
-        (0,1)
-    ]
+                    (-1,0),
+                    (1,0),
+                    (0,-1),
+                    (0,1)
+                ]
 
     neighbors = []
 
@@ -19,3 +19,53 @@ def get_neighbors(node, rows, cols):
 
         if 0 <= nr < rows and 0 <= nc < cols:
             neighbors.append((nr,nc))
+    
+    return neighbors
+
+
+def bfs(rows, cols, start, end):
+
+    queue = deque([(start,[start])])
+
+    visited = {start}
+
+    while queue:
+
+        node, path = queue.popleft()
+
+        if node == end:
+            return path
+
+        for neighbor in get_neighbors(node, rows, cols):
+
+            if neighbor not in visited:
+
+                visited.add(neighbor)
+
+                queue.append((neighbor, path + [neighbor]))
+
+    return []
+
+def dijkstra(rows, cols, start, end):
+
+    heap = [(0,start)]
+
+    distances = {start:0}
+
+    parent = {start:None}
+
+    while heap:
+
+        cost,node = heapq.heappop(heap)
+
+        if node == end:
+
+            path = []
+
+            while node:
+
+                path.append(node)
+
+                node = parent[node]
+
+            return path[::-1]
