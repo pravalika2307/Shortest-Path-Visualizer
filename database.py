8f936ec7f83c86aab0fcefb097287f0fdff45f8f
Pravalika <pravalikareddy315@gmail.com>
2026-06-06 18:44:26 +0530

    SQLite database integration

---
+++ database.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+DB_NAME = "path_history.db"
+
+def create_table():
+
+    conn = sqlite3.connect(DB_NAME)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS runs
+                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    algorithm TEXT,
+                    start_node TEXT,
+                    end_node TEXT,
+                    path_length INTEGER,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+                """
+                )
+
+    conn.commit()
+
+    conn.close()
+
+def save_run(algorithm, start_node, end_node, path_length):
+
+    conn = sqlite3.connect(DB_NAME)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""INSERT INTO runs
+                   (algorithm,
+                   start_node,
+                   end_node,
+                   path_length)
+                   VALUES(?,?,?,?)""",
+                (algorithm,
+                str(start_node),
+                str(end_node),
+                path_length)
+                )
+
+    conn.commit()
+
+    conn.close()
